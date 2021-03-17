555ba4b15a6f677d3b0f54a4f739dd9f42828c0b
Wizacass <visak.pet0@gmail.com>
2021-03-17 23:26:22 +0200

    Tweak layout of Article cell

--- NewsApp/Views/ArticleCell.swift
+++ NewsApp/Views/ArticleCell.swift
@@ -2,24 +2,39 @@ import UIKit
 import SDWebImage
 
 class ArticleCell: UITableViewCell {
+
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var thumbnailView: UIImageView!
     @IBOutlet weak var dateLabel: UILabel!
 
+    private let dateFormat = "yyyy-MM-dd"
+
     var article: Article? {
         didSet {
             titleLabel.text = article?.title
             descriptionLabel.text = article?.description
-            dateLabel.text = "Published at: \(article?.publishedAt.description)"
+            dateLabel.text = "Published at: \(formatDate())"
 
             loadImage()
         }
     }
 
     private func loadImage() {
-        let url = URL(string: article?.urlToImage ?? "")!
+        guard let urlString = article?.urlToImage else { return }
 
-        thumbnailView.sd_setImage(with: url)
+        thumbnailView.sd_setImage(
+            with: URL(string: urlString)!,
+            placeholderImage: UIImage(named: "xmark.square")
+        )
+    }
+
+    private func formatDate() -> String {
+        guard let date = article?.publishedAt else { return "" }
+
+        let formatter = DateFormatter()
+        formatter.dateFormat = dateFormat
+
+        return formatter.string(from: date)
     }
 }
