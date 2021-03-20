e132aac24382b8b6a2261ab8b83603fa62ec8156
Wizacass <visak.pet0@gmail.com>
2021-03-20 18:37:26 +0200

    Cleanup String Extensions

--- NewsApp/Extensions/StringExtensions.swift
+++ NewsApp/Extensions/StringExtensions.swift
@@ -3,11 +3,14 @@ import Foundation
 extension String {
     var htmlToAttributedString: NSAttributedString? {
         guard let data = data(using: .utf8) else { return nil }
-        do {
-            return try NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.html, .characterEncoding:String.Encoding.utf8.rawValue], documentAttributes: nil)
-        } catch {
-            return nil
-        }
+        return try? NSAttributedString(
+            data: data,
+            options: [
+                .documentType: NSAttributedString.DocumentType.html,
+                .characterEncoding: String.Encoding.utf8.rawValue
+            ],
+            documentAttributes: nil
+        )
     }
     var htmlToString: String {
         return htmlToAttributedString?.string ?? ""
